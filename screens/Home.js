d445081abaed84eff8e5fcd636fc8edbbe59cbb2
ZH-Jihan <zakirhossain19920@gmail.com>
2022-10-01 23:11:46 +0600

    complete restaurant and tabs/navigate page work

--- screens/Home.js
+++ screens/Home.js
@@ -580,6 +580,21 @@ const Home = ({ navigation }) => {
     )
 }
 
-
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: COLORS.lightGray4
+    },
+    shadow: {
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+        elevation: 1,
+    }
+})
 
 export default Home
